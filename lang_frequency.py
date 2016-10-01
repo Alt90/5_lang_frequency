d24d9bf0624dd3ca5fd93d1e2d4011ddfa6d8491
Alt13 <Alt90@rambler.ru>
2016-10-01 13:05:44 +0300

    Поправил работу с колекцией; Сделал функцию вывода словаря

--- lang_frequency.py
+++ lang_frequency.py
@@ -10,27 +10,17 @@ def load_data(filepath):
         return data_file.read()
 
 
-def get_text_lower_words(text):
-    return text.lower()
+def get_words(text):
+    return re.findall(r'[а-яёЁА-ЯA-z\']+', text.lower())
 
 
-def get_list_words(text):
-    return re.findall(r'[а-яёЁА-ЯA-z\']+', get_text_lower_words(text))
+def get_most_frequent_words(text, count_words=10):
+    words = get_words(text)
+    return Counter(words).most_common(count_words)
 
 
-def get_dict(text):
-    word_dict = Counter()
-    for word in get_list_words(text):
-        word_dict[word] += 1
-    return dict(word_dict)
-
-
-def sort_dict(text_dict):
-    return sorted(text_dict.items(), key=operator.itemgetter(1), reverse=True)
-
-
-def get_most_frequent_words(text_dict, count_words=10):
-    for key, value in sort_dict(text_dict)[:count_words]:
+def show_dict(dict_obj):
+    for key, value in dict_obj:
         print(u'%s %s' % (key, value))
 
 
@@ -38,6 +28,5 @@ if __name__ == '__main__':
     if (len(sys.argv) < 2):
         print("File don`t enter. Please run script with argv.")
         sys.exit()
-    text = load_data(sys.argv[1])
-    text_dict = get_dict(text)
-    get_most_frequent_words(text_dict)
+    most_frequent_words = get_most_frequent_words(load_data(sys.argv[1]))
+    show_dict(most_frequent_words)
